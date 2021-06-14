b19a7ac1c92b72899b4eb20e043905a807fb044c
natta2021 <githubnatta@gmail.com>
2021-06-14 09:31:43 +0200

    Add project files.

---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Expense")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        //Mustn't be negative
+        [Range(1, int.MaxValue,ErrorMessage ="Amount must be greater than 0!")]
+        public int Amount { get; set; }
+    }
+}
